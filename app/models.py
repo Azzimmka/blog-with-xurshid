3c6dba7cacef627a1f7c415e149efd7b37cbbf56
azimbek <voron75voron@gmail.com>
2026-05-07 08:38:06 +0500

    models added

--- app/models.py
+++ app/models.py
@@ -3,3 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='articles/')
+    view = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
