6db78b061ba2df89fcb2e45f724ca889bc59e016
azimbek <voron75voron@gmail.com>
2026-05-07 09:48:26 +0500

    закончили 2 модельки и view + urls

--- app/models.py
+++ app/models.py
@@ -6,11 +6,27 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255)
     
+    class Meta:
+        verbose_name = 'Katigoriya'
+        verbose_name_plural = 'Katigoriyalar'
+        
+    def __str__(self):
+        return self.title
+    
 class Article(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='articles/')
-    view = models.IntegerField()
+    image = models.ImageField(upload_to='articles')
+    view = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    
+    
+    class Meta:
+        verbose_name = 'Xavola'
+        verbose_name_plural = 'Xavolalar'
+        
+        
+    def __str__(self):
+        return self.title
--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+
+
+def home(request):
+    articles = Article.objects.all()
+    
+    return render(request, 'index.html', {'articles': articles})
